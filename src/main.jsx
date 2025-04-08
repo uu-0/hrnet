b91eb59e7006566a10624b1ecd0764ac28644967
uu-0 <louisepop14@gmail.com>
2025-04-08 16:47:28 +0200

    ajout du package react-modal-component-uu0 #2

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 
-
-import styled from 'styled-components'
 import { createGlobalStyle } from 'styled-components'
 import { colors } from './styles/colors'
 import { montserratFont } from './styles/font'
@@ -23,7 +21,7 @@ const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: black;
-    background: linear-gradient(90deg, ${colors.pink}, ${colors.orange});
+    background: linear-gradient(90deg, ${colors.blue}, ${colors.pink}, ${colors.orange});
     margin-top: 100px;
   }
 
