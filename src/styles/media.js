8ee7acfb11f3c44101493167a976eba854a5829e
uu-0 <louisepop14@gmail.com>
2025-04-15 15:58:56 +0200

    ajout du fichier media contenant les breakpoints

---
+++ src/styles/media.js
@@ -0,0 +1,20 @@
+export const breakpoints = {
+    mobileS: '320px',
+    mobileM: '375px',
+    mobileL: '414px',
+    tablet: '768px',
+    laptop: '1024px',
+    desktop: '1440px',
+    desktopL: '1920px'
+  }
+  
+export const device = {
+    mobileS: `(max-width: ${breakpoints.mobileS})`,
+    mobileM: `(max-width: ${breakpoints.mobileM})`,
+    mobileL: `(max-width: ${breakpoints.mobileL})`,
+    tablet: `(max-width: ${breakpoints.tablet})`,
+    laptop: `(max-width: ${breakpoints.laptop})`,
+    desktop: `(max-width: ${breakpoints.desktop})`,
+    desktopL: `(max-width: ${breakpoints.desktopL})`
+  }
+  
